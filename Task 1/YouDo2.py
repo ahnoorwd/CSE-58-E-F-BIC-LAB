92abdc6e250675f9101fbd1397ac5637da6274ab
Abdun Nur <abdunn951@gmail.com>
2026-05-19 16:12:39 +0600

    Delete Task 1/YouDo2.py

--- Task 1/YouDo2.py
+++
@@ -1,8 +0,0 @@
-age = 25          
-height = 5.9     
-name = "Alice"
-greeting = f"Hello, my name is {name} and I am {age} years old."
-
-print(greeting)
-print("Type of age:", type(age))
-print("Type of height:", type(height))
