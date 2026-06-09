4d4d98f4349af4beb30130237945795af4d99deb
Abdun Nur <abdunn951@gmail.com>
2026-06-09 16:12:12 +0600

    Update Problem2.py

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,16 @@
 
+text = "ACGTTGCATGTCGCATGATGCATGAGAGCT"
+k = 4   
+count = {}
+
+for i in range(len(text) - k + 1):
+    pattern = text[i:i + k]
+    if pattern in count:
+        count[pattern] += 1
+    else:
+        count[pattern] = 1
+max_count = max(count.values())
+
+for pattern in count:
+    if count[pattern] == max_count:
+        print(pattern, end=" ")
